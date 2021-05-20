1f1d4676f17feabeba010b032fd4510d53764601
Lisek95 <83589281+Lisek95@users.noreply.github.com>
2021-05-20 13:45:28 +0200

    Update delta.ts

--- delta.ts
+++ delta.ts
@@ -1,14 +1,8 @@
-const tablica: number[] = [1,2,3,2,4,6];
-const delta = (tablica[1]*tablica[1]) - (4*tablica[0]*tablica[2]);
-console.log("Delta wynosi: " + delta);
+const tablica: number[] = [1,2,3,2,4,6];  /// dodaje tablice
+const delta = (tablica[1]*tablica[1]) - (4*tablica[0]*tablica[2]);    /// jedno do kwadratu - to razy to drugie
+console.log("Delta =: " + delta);   /// wynik
 
-if (delta === 0)
-{
-    const x0 = -tablica[1] / 2 * tablica[0];
-    console.log("Delta ma jedno miejsce zerowe: " + x0)
-}
-
-if (delta > 0)
+if (delta > 0) /// jeżeli delta większa od 0
 {
     const x1 = (-tablica[1] - Math.sqrt(delta)) / (2 * tablica[0])
     const x2 = (-tablica[1] + Math.sqrt(delta)) / (2 * tablica[0])
@@ -16,7 +10,13 @@ if (delta > 0)
     console.log("x2 = " + x2)
 }
 
-if (delta < 0)
+if (delta === 0) /// jeżeli delta = 0
 {
-    console.log("brak miejsc zerowych");
+    const x0 = -tablica[1] / 2 * tablica[0];
+    console.log("Delta ma jedno miejsce zerowe: " + x0)
+}
+
+if (delta < 0)   /// jeżeli delta mniejsza od 0
+{
+    console.log("delta nie ma miejsc zerowych);
 }
