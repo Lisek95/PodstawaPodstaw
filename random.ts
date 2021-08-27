626d54618999f1a57db25acf83cc19c1b79c9b69
Lisek95 <83589281+Lisek95@users.noreply.github.com>
2021-08-28 01:45:44 +0200

    Add files via upload

---
+++ random.ts
@@ -0,0 +1,9 @@
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+console.log(Math.random(), Math.random());
+if (min > 0.5) { min = 1 }
+if (min < 0.5) { min = 0 }
+console.log(min, Math.random());
